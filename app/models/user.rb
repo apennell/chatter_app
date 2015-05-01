81c7c7305627c75ea6f4dc378952ead88f2f9be7
Annie Pennell <annie@anniepennell.com>
2015-05-01 12:26:17 -0700

    Added BCrypt, created routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,34 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
   has_many :posts
+  BCrypt::Engine.cost = 12
+
+  attr_reader :password
+
+  validates_confirmation_of :password
+  validates_presence_of :password_digest
+
+  def password=(unencrypted_password)
+    if unencrypted_password.nil?
+      self.password_digest = nil
+    else
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(@password)
+    end
+  end
+
+  def authenticate(unencrypted_password)
+    if BCrypt::Password.new(password_digest) == unencrypted_password
+      self
+    else
+      false
+    end
+  end
+
+  def self.confirm(username_param, password_param)
+    user = User.find_by({username: username_param})
+    user.authenticate(password_param)    
+  end
+
 end
